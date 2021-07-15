75afe029d78bb809c50055ae9dfafdb47338ea2c
TretasPt <86566152+TretasPt@users.noreply.github.com>
2021-07-15 13:31:34 +0100

    Adicionado um temporizador na versão python.

--- V....py
+++ V....py
@@ -4,8 +4,14 @@
 #     print("m", end ="")
 #     i += 1
 # print(".")
+import time
+
+t0 = time.perf_counter()
 
 print ("G", end = "")
 for i in range (65517):
     print ("o", end = "")
 print("d morning Vietnam!")
+
+t1 = time.perf_counter()
+print(t1-t0)

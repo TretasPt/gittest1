9453e144ca644c64398c531e1d5b0bc030b48d44
TretasPt <86566152+TretasPt@users.noreply.github.com>
2021-07-15 13:08:35 +0100

    Adicionados os ficheiros de base do programa "V..." em pascal e python

---
+++ V....pas
@@ -0,0 +1,9 @@
+Program Pzim ;
+var i:integer;
+Begin
+	write('G');
+  for i := 1 to 65517 do
+		write ('o');
+	write('d morning Vietnam!')	
+End.
+
---
+++ V....py
@@ -0,0 +1,11 @@
+# i = 0
+# print("H", end = "")
+# while i < 1998:
+#     print("m", end ="")
+#     i += 1
+# print(".")
+
+print ("G", end = "")
+for i in range (65517):
+    print ("o", end = "")
+print("d morning Vietnam!")
